d9adb6274ebff9aa4115c2ff5c272331d4c07a9b
HenryZhu123 <41032770+HenryZhu123@users.noreply.github.com>
2019-11-28 17:05:47 -0500

    First Draft

--- js/file.js
+++ js/file.js
@@ -1,69 +1,105 @@
 //Main function to generate table. Everything goes through this function.
-function generateTable(multplierMin, multplierMax, multiplicandMin, multiplicandMax) {
+function generateTable() {
   var table = document.getElementById("resultTable");
   table.innerHTML = ""; //Clears table contents in case there was previously cells in there
 
-  var multiplier1 = multplierMin.value;
-  var multiplier2 = multplierMax.value;
-  var multiplicand1 = multiplicandMin.value;
-  var multiplicand2 = multiplicandMax.value;
-
-  //Pass as strings to handle input "-"
-//  if(validateInput(multiplier1, multiplier2, multiplicand1, multiplicand2) == false){
-//    return;
-//  }
+  var multiplier1 = parseInt(document.getElementById("multiplier1").value);
+  var multiplier2 = parseInt(document.getElementById("multiplier2").value);
+  var multiplicand1 = parseInt(document.getElementById("multiplicand1").value);
+  var multiplicand2 = parseInt(document.getElementById("multiplicand2").value);
 
   //convert to Number to handle floating point inputs
-  multiplier1 = Number(multiplier1);
-  multiplier2 = Number(multiplier2);
-  multiplicand1 = Number(multiplicand1);
-  multiplicand2 = Number(multiplicand2);
+  if (multiplier1 <= multiplier2) {
+    multiplierMin = Number(multiplier1);
+    multiplierMax = Number(multiplier2);
+  } else {
+    multiplierMin = Number(multiplier2);
+    multiplierMax = Number(multiplier1);
+  }
+
+  if (multiplicand1 <= multiplicand2) {
+    multiplicandMin = Number(multiplicand1);
+    multiplicandMax = Number(multiplicand2);
+  } else {
+    multiplicandMin = Number(multiplicand2);
+    multiplicandMax = Number(multiplicand1);
+  }
+
 
   var rowHead = table.insertRow(0);
   var cell = rowHead.insertCell(-1);
   cell.innerHTML = ''; //Fills in blank cell that is at the crossing point of the two headers
 
   //Fills in horizontal header
-  for (i = multiplier1; i <= multiplier2; i++) {
+  for (i = multiplierMin; i <= multiplierMax; i++) {
     console.log(i);
     var cell = rowHead.insertCell(-1);
     cell.innerHTML = i;
   }
 
   //Fills in cells
-  for (i = multiplicand1; i <= multiplicand2; i++) {
+  for (i = multiplicandMin; i <= multiplicandMax; i++) {
     var row = table.insertRow(-1);
     var cell = row.insertCell(-1);
     cell.innerHTML = i; //Fills in vertical header
-    for (j = multiplier1; j <= multiplier2; j++) {
+    for (j = multiplierMin; j <= multiplierMax; j++) {
       var cell = row.insertCell(-1);
       cell.innerHTML = i * j;
     }
   }
-}
+};
 
-//Validates input to generateTable function
-function validateInput(multiplier1, multiplier2, multiplicand1, multiplicand2) {
-  if (multiplier1 == "" || multiplier2 == "" || multiplicand1 == "" || multiplicand2 == "") {
-    alert("You must enter a number in all of the boxes!");
-    return false;
-  }
+function validate() {
+  $("#tableInput").validate({
+    // Rules for validating the form.
+    rules: {
+      multiplier1: {
+        required: true,
+        integer: true,
+      },
+      multiplier2: {
+        required: true,
+        integer: true,
+      },
+      multiplicand1: {
+        required: true,
+        integer: true,
+      },
+      multiplicand2: {
+        required: true,
+        integer: true,
+      }
+    },
 
-  //Convert to Number to compare numerical value rather than first digit
-  multiplier1 = Number(multiplier1);
-  multiplier2 = Number(multiplier2);
-  multiplicand1 = Number(multiplicand1);
-  multiplicand2 = Number(multiplicand2);
+    messages: {
+      multiplier1: {
+        required: "This entry cannot be blank. Please enter an integer.",
+        integer: "The value must be an integer. Please enter an integer."
+      },
+      multiplier2: {
+        required: "This entry cannot be blank",
+        integer: "The value must be an integer. Please enter an integer."
+      },
+      multiplicand1: {
+        required: "This entry cannot be blank",
+        integer: "The value must be an integer. Please enter an integer."
+      },
+      multiplicand2: {
+        required: "This entrycannot be blank",
+        integer: "The value must be an integer. Please enter an integer."
+      }
+    },
 
-  if (multiplier1 > multiplier2) {
-    alert("The first multiplier must be less than or equal to the second multiplier!");
-    return false;
-  }
-  else if (multiplicand1 > multiplicand2) {
-    alert("The first multiplicand must be less than or equal to the second multiplicand!");
-    return false;
-  }
-  else {
-    return true;
-  }
-}
+    // Handler for submit button click
+    submitHandler: function() {
+      generateTable();
+      return false;
+    },
+
+    //Changes location of error message
+    errorElement: "div",
+    errorPlacement: function(error, element) {
+      error.insertAfter(element);
+    }
+  });
+};
